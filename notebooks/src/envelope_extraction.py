b41b8da04daef685581e746588bce1dfac865ef6
dragonprevost <dragon@dkp.io>
2019-11-26 19:27:59 -0800

    formatted

--- notebooks/src/envelope_extraction.py
+++ notebooks/src/envelope_extraction.py
@@ -12,7 +12,7 @@ def three_step_envelope(signal, K=200, freq_cutoff=0.125, padlen=150):
     N = len(abs_signal)
     peaks = []
     for i in range(0, N, K):
-        peaks.append(np.max(abs_signal[i:i+K]))
+        peaks.append(np.max(abs_signal[i : i + K]))
     peaks = np.asarray(peaks)
 
     # Apply a LPF with a cut off of 0.125 Times the Niquist (125 Hz)
@@ -37,17 +37,17 @@ def find_first_onset(signal, sr, K=200):
 
 
 def attack(signal, onset, sr):
-    '''
+    """
     Computes attack time by taking the difference 
     of the argmax and onset and dividing by the sampling rate
-    '''
-    return (np.argmax(np.abs(signal)) - onset)/sr
+    """
+    return (np.argmax(np.abs(signal)) - onset) / sr
 
 
 def extract_envelope(signal, sr):
-    '''
+    """
     Approximate envelope, Extract attack
-    '''
+    """
     K = 200
     approx_env = three_step_envelope(signal=signal, K=K)
     # convert envelope to Fortran-contiguous
@@ -56,9 +56,4 @@ def extract_envelope(signal, sr):
     first_onset = find_first_onset(signal, sr=sr)
     attack_t = attack(ftrn_env, first_onset, sr)
 
-    return {
-        "attack": attack_t,
-        "decay": 0.5,
-        "sustain": (1.0, 0.5),
-        "release": 2.0,
-    }
+    return {"attack": attack_t, "decay": 0.5, "sustain": (1.0, 0.5), "release": 2.0}
--- notebooks/src/synth.py
+++ notebooks/src/synth.py
@@ -5,8 +5,8 @@ import numpy as np
 
 # 3. build out get_signal
 
-class Synth():
 
+class Synth:
     def __init__(self, signal=[], srate=44100):
 
         self.signal = signal
@@ -18,7 +18,7 @@ class Synth():
             "attack": 0.0,
             "decay": 0.0,
             "sustain": (0.0, 0.0),
-            "release": 0.0
+            "release": 0.0,
         }
 
     def set_srate(self, srate):
@@ -28,8 +28,10 @@ class Synth():
         return srate
 
     def set_envelope(self, envelope):
-        sustain_time, sustain_power = envelope['sustain']
-        self.duration = envelope['attack'] + envelope['decay'] + envelope['release'] + sustain_time
+        sustain_time, sustain_power = envelope["sustain"]
+        self.duration = (
+            envelope["attack"] + envelope["decay"] + envelope["release"] + sustain_time
+        )
         self.envelope = envelope
 
     def get_envelope(self):
@@ -44,10 +46,12 @@ class Synth():
     def set_frequencies(self, signal=[]):
         if signal == []:
             signal = self.signal
-        freqs = [(107.666015625, 11.412091),
-                    (215.33203125, 14.899792),
-                    (333.7646484375, 9.119412),
-                    (441.4306640625, 4.1135097)]
+        freqs = [
+            (107.666015625, 11.412091),
+            (215.33203125, 14.899792),
+            (333.7646484375, 9.119412),
+            (441.4306640625, 4.1135097),
+        ]
         self.frequencies = freqs
         return freqs
 
@@ -57,8 +61,18 @@ class Synth():
     def envelope_amp_track(self):
         return envelope_amplitude(self.envelope, self.srate)
 
-    def oscillator(self, freq=440.0, dur=1.0, srate=44100.0, amp=1.0, phase=0.0, wave_type='sinusoid'):
-        return oscillator(freq=freq, dur=dur, srate=srate, amp=amp, phase=phase, wave_type=wave_type)
+    def oscillator(
+        self,
+        freq=440.0,
+        dur=1.0,
+        srate=44100.0,
+        amp=1.0,
+        phase=0.0,
+        wave_type="sinusoid",
+    ):
+        return oscillator(
+            freq=freq, dur=dur, srate=srate, amp=amp, phase=phase, wave_type=wave_type
+        )
 
     def set_signal(self, signal):
         self.signal = signal
@@ -76,8 +90,13 @@ class Synth():
         output = np.zeros(int(self.srate * self.duration))
 
         for osc in oscillators:
-            output += oscillator(freq=osc['freq'], dur=self.duration, amp=osc['amp'], phase=osc['phase'], wave_type=osc['wave_type'])
+            output += oscillator(
+                freq=osc["freq"],
+                dur=self.duration,
+                amp=osc["amp"],
+                phase=osc["phase"],
+                wave_type=osc["wave_type"],
+            )
         amp_track, length = envelope_amplitude(self.envelope, self.srate)
-        
-        return output[:length] * amp_track 
 
+        return output[:length] * amp_track
